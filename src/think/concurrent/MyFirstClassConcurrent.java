d4bbca49fef6f5c46457814401761819af5dc51d
Gianluca Consoli <gianluca.consoli@studio.unibo.it>
2021-11-25 19:34:28 +0100

    Created a Clock class that show milliseconds

--- src/think/concurrent/MyFirstClassConcurrent.java
+++ src/think/concurrent/MyFirstClassConcurrent.java
@@ -2,6 +2,30 @@ package think.concurrent;
 
 import java.util.concurrent.*;
 
-public class MyFirstClassConcurrent {
+/**
+ * 
+ *  This is a Clock that use a Thread Class
+ *
+ */
+public class MyFirstClassConcurrent extends Thread {
+    
+    private double millisecond;
+    //final private java.util.Date d = new java.util.Date();
+    
+    public MyFirstClassConcurrent () {
+        this.millisecond = new java.util.Date().getTime();
+    }
+    
+    public void run() {
+        while(true) {
+            this.millisecond = new java.util.Date().getTime();
+            System.out.println("millesecond : " + this.millisecond);
+            try {
+                sleep(10);
+            } catch (Exception e) {
+                
+            }
+        }
+    }
 
 }
