521f03b5ee1b0c4d20f7cb8a6cd715ded8fbea3f
Gianluca Consoli <gianluca.consoli@studio.unibo.it>
2021-11-26 19:24:58 +0100

    Created my second clock that implement <<Runnable>>

---
+++ src/think/concurrent/ClockRunnable.java
@@ -0,0 +1,31 @@
+package think.concurrent;
+
+import java.util.concurrent.*;
+
+/**
+ * 
+ *  My Second Version of Clock
+ *
+ */
+public class ClockRunnable implements Runnable {
+
+    private double millisecond;
+    //Thread th = new Thread(new ClockRunnable());
+    
+    public ClockRunnable () {
+        this.millisecond = new java.util.Date().getTime();
+    }
+        
+    public void run() {
+        while(true) {
+            this.millisecond = new java.util.Date().getTime();
+            System.out.println("millesecond : " + this.millisecond);
+            try {
+                Thread.sleep(1000);
+            } catch (Exception e) {
+                
+            }
+        }
+    }
+    
+}
