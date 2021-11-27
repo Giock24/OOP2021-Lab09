02f8ae76657d4f5b00834612977c6a071a0d01c9
Gianluca Consoli <gianluca.consoli@studio.unibo.it>
2021-11-27 18:56:00 +0100

    Created two class that must synch each other

---
+++ src/think/concurrent/Agent1.java
@@ -0,0 +1,34 @@
+package think.concurrent;
+
+public class Agent1 extends Thread{
+    
+    private final MySimpleSynchronizer sync;
+    
+    public Agent1(final MySimpleSynchronizer sync) {
+        this.sync = sync;
+    }
+    
+    public void run() {
+        this.a1();
+        this.sync.signaArrived();
+        this.a2();
+        this.a3();
+    }
+    
+    public void a1() {log("a1"); delay(5000);}
+    public void a2() {log("a2"); delay(2000);}
+    public void a3() {log("a3"); delay(1000);}
+
+    private void log(final String string) {
+        System.out.println("[Thread-01] : " + string);
+    }
+    
+    public void delay(final long ms) {
+        try {
+            Thread.sleep(ms);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
---
+++ src/think/concurrent/Agent2.java
@@ -0,0 +1,38 @@
+package think.concurrent;
+
+public class Agent2 extends Thread{
+    
+private final MySimpleSynchronizer sync;
+    
+    public Agent2(final MySimpleSynchronizer sync) {
+        this.sync = sync;
+    }
+    
+    public void run() {
+        this.b1();
+        try {
+            this.sync.waitForSignal();
+            this.b2();
+            this.b3();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    public void b1() {log("b1");}
+    public void b2() {log("b2");}
+    public void b3() {log("b3");}
+
+    private void log(final String string) {
+        System.out.println("[Thread-02] : " + string);
+    }
+    
+    public void delay(final long ms) {
+        try {
+            Thread.sleep(ms);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
